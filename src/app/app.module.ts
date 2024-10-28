703b6d55b3377dbcccbce4a804158e430e77be89
unknown <elaarfaoui.yassine2@gmail.com>
2024-10-28 21:53:57 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { BodyCardComponent } from './body-card/body-card.component';
+import { EtablissementComponent } from './etablissement/etablissement.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageComponent
+    PageComponent,
+    HeaderComponent,
+    BodyComponent,
+    BodyCardComponent,
+    EtablissementComponent
   ],
   imports: [
     BrowserModule
